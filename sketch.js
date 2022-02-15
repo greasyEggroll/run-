ee5f7db49e16cf7f45a15749baea93ccc4baa6ac
greasyEggroll <77175104+greasyEggroll@users.noreply.github.com>
2022-02-14 19:29:55 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,26 +1,50 @@
-var img
-var runner 
+var path
+var runner
+var InvisibleBoundaryLeft
+var InvisbleBoundaryRight 
+
 
 function preload(){
   //pre-load images
-  img = loadImage('path.png');
-  runner = loadImage('Runner-1.png')
+  pathImg = loadImage('path.png');
+  runnerImg = loadAnimation('Runner-1.png','Runner-2.png');
 
 }
 
 function setup(){
+  createCanvas(400,400)
   //create sprites here
-  img.velocityY = 4
-  img.scale = 1.2
+  InvisibleBoundaryLeft = createSprite(0,0,100,800)
+  InvisibleBoundaryLeft.visible = false
+  InvisibleBoundaryRight = createSprite(400,400,100,800)
+  InvisibleBoundaryRight.visible = false
 
-  if (img.y > 400) {
-    img.y = height/2
-  }
+  path = createSprite(200,200)
+  path.addImage(pathImg)
+  path.velocityY = 4
+  path.scale = 1.2
+
+  runner = createSprite(200,340)
+  runner.addAnimation("running", runnerImg);
+  runner.scale = 0.08
+
 }
   
 
 function draw() {
   background(0);
+  edges = createEdgeSprites()
+  path.velocityY = 4
+
+  runner.x = World.mouseX
+  runner.collide(edges[3])
+  runner.collide(InvisibleBoundaryLeft)
+  runner.collide(InvisibleBoundaryRight)
+
+  if (path.y > 400) {
+    path.y = height/2
+  }
   drawSprites()
-  image(img,0,0)
+
+  
 }
